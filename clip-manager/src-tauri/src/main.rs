6ad1e8189caddbc58aa008b849fabf98a8bfda97
RainbowPigCake <kaitlynwong97@gmail.com>
2023-04-11 22:41:18 -0400

    remove default tauri command from backend

--- clip-manager/src-tauri/src/main.rs
+++ clip-manager/src-tauri/src/main.rs
@@ -2,14 +2,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! Hello World!!", name)
-}
+// #[tauri::command]
+
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
