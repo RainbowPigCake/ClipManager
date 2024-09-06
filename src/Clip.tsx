ba8ad19413ae7e257affe413aca068ac8465f913
RainbowPigCake <kaitlynwong97@gmail.com>
2024-09-06 08:39:49 -0400

    update placeholder thumbnail image path

--- src/Clip.tsx
+++ src/Clip.tsx
@@ -15,8 +15,7 @@ export default function Clip({ filePath, handleClickClip }) {
       <CardHeader>
         <img
           className="rounded-md object-scale-down w-[276px] h-[155px] mb-1"
-          src="https://media.discordapp.net/attachments/1279640411236991090/1280609307070697564/IMG_20240903_142316.jpg?ex=66d8b3db&is=66d7625b&hm=3afc5dc77bf6957a0a5164a9e3889ed208dc16641457003458d999d0e7cf611e&=&format=webp&width=507&height=676"
-        />
+          src="https://static.wikia.nocookie.net/silly-cat/images/7/78/Melon_Cat_Species_2.png" />
       </CardHeader>
       <CardContent>
         <CardTitle>
