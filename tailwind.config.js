3f8daaddd46907b1518ac38a2a5fde06cebab9df
RainbowPigCake <kaitlynwong97@gmail.com>
2024-09-05 22:59:57 -0400

    initialize shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,17 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: true,
   theme: {
-    extend: {
-      fontFamily: {
-        'Inter': ['Inter', 'times new roman']
-      }
-    },
+  	extend: {
+  		fontFamily: {
+  			Inter: ['Inter', 'times new roman']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
