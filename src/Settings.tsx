ababdf589d26b91b0703e6e29429fb77c2070305
Kaitlyn <kaitlynwong97@gmail.com>
2025-02-21 01:12:28 -0500

    Settings (#27)
    * low thumbnail batch size temporarily
    
    * update test path
    
    * temp
    
    * beta clip viewer UI update
    
    * manage enter and exiting settings page
    
    * update infra before adding settings page
    
    * init settings v2

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,10 +1,16 @@
 import { useRef, useEffect } from 'react';
+import { Card } from '@/components/ui/card';
+import { Toggle } from '@/components/ui/toggle';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { Input } from "@/components/ui/input"
 
 export default function Settings({ }) {
 
     return (
         <>
-            Hello World
+            <div className="p-6 mr-16">
+
+            </div>
         </>
     );
 }
